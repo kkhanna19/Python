e89ce62866055879da0498f765e6c2fd5ff8c134
kkhanna19 <kajalkhanna271@gmail.com>
2025-10-06 10:45:21 +0530

    first commit

---
+++ loops/Functions.py/sum.py
@@ -0,0 +1,7 @@
+def cal_sum(a, b):
+    sum = a + b
+    print(sum)
+    return sum
+
+cal_sum(2, 3)
+cal_sum(4, 4)
--- loops/factorial.py
+++ loops/factorial.py
@@ -1,6 +1,13 @@
 n = 5
 fact = 1
 
+i = 1
+while i <= n:
+    fact *= i
+    i +=1
+
+print(fact)
+
 for i in range(1, n+1):
    fact *= i
 
